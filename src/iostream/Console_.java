8919bd5e88bb32b4180d146d6dbe3f4d42f9d71c
백광민 <carpediem2001@nate.com>
2024-10-15 16:59:15 +0900

    feat : API활용 Java & Spring 백엔드 과정 10일차
    <IO 기반 네트워킹>
    (java.io package)
    입출력 스트림 개념정의,-InputStream/OutputStream(Reader, Writer,Console, File,)

---
+++ src/iostream/Console_.java
@@ -0,0 +1,20 @@
+package iostream;
+import java.io.Console;
+
+public class Console_ {
+    public static void main(String[] args) {
+        Console console = System.console();
+
+        System.out.print("ID : ");
+        String id = console.readLine();
+
+        System.out.print("PASSWORD : ");
+        char[] charPass = console.readPassword();
+        String strPassword = new String(charPass);
+
+        System.out.println("------------------------");
+        System.out.println(id);
+        System.out.println(strPassword);
+
+    }
+}
