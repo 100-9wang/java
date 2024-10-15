95f07904c8d556ae80c613d1be34925ef2c1eeb5
백광민 <carpediem2001@nate.com>
2024-10-15 15:29:11 +0900

    feat : API활용 Java & Spring 백엔드 과정 9일차
    <API>
    - 기본 API class (java.lang, java.util package)
    - Generic
    제네릭 상속 및 구현

---
+++ src/generic/HighStudent.java
@@ -0,0 +1,31 @@
+//package generic;
+//
+//
+//class Person {
+//    private String name;
+//
+//    public Person(String name){
+//        this.name = name;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return name;
+//    }
+//
+//class Student extends Person {
+//        public Student(String name){
+//            super(name);
+//        }
+//}
+//
+//class Worker extends Person {
+//        public Worker(String name){
+//            super(name);
+//        }
+//}
+//}
