505b064112b6f2fb52cefaa98a0c7d5d936e1bfc
백광민 <carpediem2001@nate.com>
2024-10-28 18:09:16 +0900

    feat : 게시판 만들기 <1차>
    -메인메뉴 기능 구현
    -게시글 추가, 수정, 삭제 기능 추가
    -종료 기능 추가
    -회원가입 기능 추가 (중복성 , 이메일 포멧 검사)

--- src/report/function_boards.java
+++ src/report/function_boards.java
@@ -9,7 +9,9 @@ public class function_boards {
     users users = new users();
     private Connection conn;
     private Connection1 con;
-    private boolean isSignedUp = false;
+    private boolean signin = false;
+    private boolean login = false;
+    private String login_userID;
 
     //boards
     public function_boards() {
@@ -19,9 +21,9 @@ public class function_boards {
 
     public void list() {
         System.out.println("[ 광민 게시판 ]");
-        System.out.println("-------------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------------------------");
         System.out.printf("%-6s%-12s%-14s%-23s%-29s\n", "번호", "글쓴이", "주제", "날짜", "제목");
-        System.out.println("-------------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------------------------");
 
         try {
             String sql = "SELECT b_no, b_writer, b_content, b_title, b_date FROM boards";
@@ -50,45 +52,80 @@ public class function_boards {
         main_menu();
     }
 
-    public void main_menu() {
+    public void user_menu() {
+        if (login == false) {
+            log_in();
+        } else {
+            while (true) {
+                System.out.println("-------------------------------------------------------------------------------------");
+                System.out.println("회원 메뉴 : 1. 정보 수정 | 2. 회원 탈퇴 | 3. 메인 메뉴 | 4. 로그아웃 | 0. 종료 |");
+                System.out.print("메뉴를 선택하세요 : ");
+                String inputNum = s.nextLine();
+                System.out.println("-------------------------------------------------------------------------------------");
 
-        if (!isSignedUp) { // 회원가입이 되어 있지 않으면
-            while (!isSignedUp) {
-                isSignedUp = singUp(); // 회원가입 시도
-                if (!isSignedUp) {
-                    System.out.println("회원 가입에 실패했습니다. 다시 시도해주세요.");
+                switch (inputNum) {
+                    case "1":
+                        log_in();
+                        user_update();
+                        user_menu();
+                        break;
+                    case "2":
+                        log_in();
+                        singout();
+                        user_menu();
+                        break;
+                    case "3":
+                        list();
+                        break;
+                    case "4":
+                        logout();
+                        list();
+                        break;
+                    case "0":
+                        exit();
+                        break;
                 }
             }
         }
-        while (true){
-        System.out.println("-------------------------------------------------------------------------");
-        System.out.println("메인 메뉴 : 1. 게시글 생성 | 2. 게시글 수정 | 3. 게시글 삭제 | 4. 게시판 목록 | 0. 종료");
-        System.out.print("메뉴를 선택하세요 : ");
-        String inputNum = s.nextLine();
-        System.out.println("-------------------------------------------------------------------------");
+    }
+    public void main_menu() {
 
-        switch (inputNum) {
-            case "1":
-                create();
-                list();
-                break;
-            case "2":
-                update();
-                list();
-                break;
-            case "3":
-                delete();
-                list();
-                break;
-            case "4":
-                list();
-                break;
-            case "0":
-                exit();
-                break;
+        while (true) {
+            System.out.println("-------------------------------------------------------------------------------------");
+            System.out.println("메인 메뉴 : 1. 게시글 생성 | 2. 게시글 수정 | 3. 게시글 삭제 | 4. 게시판 목록 | 5. 회원 가입 | 6. 내 정보 | 0. 종료 |");
+            System.out.print("메뉴를 선택하세요 : ");
+            String inputNum = s.nextLine();
+            System.out.println("-------------------------------------------------------------------------------------");
+
+            switch (inputNum) {
+                case "1":
+                    create();
+                    list();
+                    break;
+                case "2":
+                    update();
+                    list();
+                    break;
+                case "3":
+                    delete();
+                    list();
+                    break;
+                case "4":
+                    list();
+                    break;
+                case "5":
+                    singUp();
+                    list();
+                    break;
+                case "6":
+                    user_menu();
+                    break;
+                case "0":
+                    exit();
+                    break;
+            }
         }
     }
-}
 
     public void exit() {
         System.out.println("게시판 프로그램을 종료합니다.");
@@ -103,75 +140,87 @@ public class function_boards {
     }
 
     public void create() {
-        System.out.println("게시글 생성을 선택하셨습니다.");
-        System.out.print("번호 : ");
-        boards.setB_no(Integer.parseInt(s.nextLine()));
-        System.out.print("글쓴이 : ");
-        boards.setB_writer(s.nextLine());
-        System.out.print("주제 : ");
-        boards.setB_content(s.nextLine());
-        System.out.print("제목 : ");
-        boards.setB_title(s.nextLine());
+        if (login == false) {
+            log_in();
+        } else {
+            System.out.println("게시글 생성을 선택하셨습니다.");
+            System.out.print("번호 : ");
+            boards.setB_no(Integer.parseInt(s.nextLine()));
+            System.out.print("글쓴이 : ");
+            boards.setB_writer(s.nextLine());
+            System.out.print("주제 : ");
+            boards.setB_content(s.nextLine());
+            System.out.print("제목 : ");
+            boards.setB_title(s.nextLine());
 
-        try {
-            String sql = "INSERT INTO boards (b_no, b_content, b_writer, b_date, b_title) VALUES (?, ?, ?, SYSDATE, ?)";
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-            pstmt.setInt(1, boards.getB_no());
-            pstmt.setString(2, boards.getB_content());
-            pstmt.setString(3, boards.getB_writer());
-            pstmt.setString(4, boards.getB_title());
+            try {
+                String sql = "INSERT INTO boards (b_no, b_content, b_writer, b_date, b_title) VALUES (?, ?, ?, SYSDATE, ?)";
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+                pstmt.setInt(1, boards.getB_no());
+                pstmt.setString(2, boards.getB_content());
+                pstmt.setString(3, boards.getB_writer());
+                pstmt.setString(4, boards.getB_title());
 
-            pstmt.executeUpdate();
-            pstmt.close();
-            System.out.println("게시글이 성공적으로 생성되었습니다.");
-        } catch (Exception e) {
-            e.printStackTrace();
+                pstmt.executeUpdate();
+                pstmt.close();
+                System.out.println("게시글이 성공적으로 생성되었습니다.");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public void update() {
-        System.out.println("게시글 수정하기를 선택하셨습니다.");
-        System.out.print("변경할 게시글의 번호를 입력해주세요 : ");
-        boards.setB_no(Integer.parseInt(s.nextLine()));
+        if (login == false) {
+            log_in();
+        } else {
+            System.out.println("게시글 수정하기를 선택하셨습니다.");
+            System.out.print("변경할 게시글의 번호를 입력해주세요 : ");
+            boards.setB_no(Integer.parseInt(s.nextLine()));
 
-        System.out.println("수정할 내용을 입력해주세요.");
-        System.out.print("글쓴이 : ");
-        boards.setB_writer(s.nextLine());
-        System.out.print("주제 : ");
-        boards.setB_content(s.nextLine());
-        System.out.print("제목 : ");
-        boards.setB_title(s.nextLine());
+            System.out.println("수정할 내용을 입력해주세요.");
+            System.out.print("글쓴이 : ");
+            boards.setB_writer(s.nextLine());
+            System.out.print("주제 : ");
+            boards.setB_content(s.nextLine());
+            System.out.print("제목 : ");
+            boards.setB_title(s.nextLine());
 
-        try {
-            String sql = "UPDATE boards SET b_writer = ?, b_content = ?, b_title = ? WHERE b_no = ?";
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, boards.getB_writer());
-            pstmt.setString(2, boards.getB_content());
-            pstmt.setString(3, boards.getB_title());
-            pstmt.setInt(4, boards.getB_no());
-            pstmt.executeUpdate();
-            pstmt.close();
-            System.out.println("게시글이 성공적으로 수정되었습니다.");
-        } catch (SQLException e) {
-            System.out.println("게시글 수정중 오류가 발생했습니다.");
-            e.printStackTrace();
+            try {
+                String sql = "UPDATE boards SET b_writer = ?, b_content = ?, b_title = ? WHERE b_no = ?";
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+                pstmt.setString(1, boards.getB_writer());
+                pstmt.setString(2, boards.getB_content());
+                pstmt.setString(3, boards.getB_title());
+                pstmt.setInt(4, boards.getB_no());
+                pstmt.executeUpdate();
+                pstmt.close();
+                System.out.println("게시글이 성공적으로 수정되었습니다.");
+            } catch (SQLException e) {
+                System.out.println("게시글 수정중 오류가 발생했습니다.");
+                e.printStackTrace();
+            }
         }
     }
 
     public void delete() {
-        System.out.println("게시글 삭제를 선택하셨습니다.");
-        System.out.print("번호 : ");
-        boards.setB_no(Integer.parseInt(s.nextLine()));
+        if (login == false) {
+            log_in();
+        } else {
+            System.out.println("게시글 삭제를 선택하셨습니다.");
+            System.out.print("번호 : ");
+            boards.setB_no(Integer.parseInt(s.nextLine()));
 
-        try {
-            String sql = "DELETE FROM boards WHERE b_no = ?";
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-            pstmt.setInt(1, boards.getB_no());
-            pstmt.executeUpdate();
-            pstmt.close();
-            System.out.println("게시글이 성공적으로 삭제되었습니다.");
-        } catch (Exception e) {
-            e.printStackTrace();
+            try {
+                String sql = "DELETE FROM boards WHERE b_no = ?";
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+                pstmt.setInt(1, boards.getB_no());
+                pstmt.executeUpdate();
+                pstmt.close();
+                System.out.println("게시글이 성공적으로 삭제되었습니다.");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -183,7 +232,6 @@ public class function_boards {
                 System.out.print("아이디 : ");
                 String user_ID = s.nextLine();
 
-                // 중복 검사
                 if (userid_dupl(user_ID)) {
                     System.out.println("이미 사용중인 아이디 입니다.");
                 } else {
@@ -204,15 +252,15 @@ public class function_boards {
                 age = Integer.parseInt(s.nextLine());
                 users.setUser_Age(age);
             } catch (NumberFormatException e) {
-                System.out.println("나이는 숫자여야 합니다. 다시 입력해주세요.");
-                return false; // 회원가입 실패
+                System.out.println("숫자로 다시 입력해주세요.");
+                return false;
             }
 
             System.out.print("이메일 : ");
             String email = s.nextLine();
-            if (!isValidEmail(email)) {
+            if (!Email_ok(email)) {
                 System.out.println("유효하지 않은 이메일입니다. 다시 입력해주세요.");
-                return false; // 회원가입 실패
+                return false;
             }
             users.setUser_Email(email);
 
@@ -226,19 +274,29 @@ public class function_boards {
             pstmt.executeUpdate();
             pstmt.close();
             System.out.println("회원 가입이 정상적으로 처리되었습니다.");
-            return true; // 회원가입 성공
+            return true;
         } catch (SQLException e) {
             e.printStackTrace();
-            return false; // SQL 예외로 인해 실패
+            return false;
         } catch (Exception e) {
             e.printStackTrace();
-            return false; // 일반 예외로 인해 실패
+            return false;
+        }
+    }
+    public void sing_up(){
+        if (!signin) {
+            while (!signin) {
+                signin = singUp();
+                if (!signin) {
+                    System.out.println("회원 가입에 실패했습니다. 다시 시도해주세요.");
+                }
+            }
         }
     }
 
-    private boolean isValidEmail(String email) {
-        String emailRegex = "^[\\w-\\.]+@[\\w-]+\\.[a-z]{2,4}$";
-        return email.matches(emailRegex);
+    private boolean Email_ok(String email) {
+        String emailok = "^[\\w-\\.]+@[\\w-]+\\.[a-z]{2,4}$";
+        return email.matches(emailok);
     }
 
     private boolean userid_dupl(String user_ID) {
@@ -252,13 +310,140 @@ public class function_boards {
                 boolean found = rs.getInt(1) > 0;
                 rs.close();
                 pstmt.close();
-                return found; // 중복이면 true
+                return found;
             }
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return false; // 중복이 아니면 false
+        return false;
+    }
+
+    public void singout() {
+        System.out.println("회원 탈퇴를 선택하셨습니다.");
+        System.out.print("아이디 : ");
+        users.setUser_ID(s.nextLine());
+        System.out.print("비밀번호 : ");
+        users.setUser_PW(s.nextLine());
+
+        try {
+            String sql = "DELETE FROM boards WHERE user_id = ?, user_pw = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, users.getUser_ID());
+            pstmt.setString(2, users.getUser_PW());
+            pstmt.executeUpdate();
+            pstmt.close();
+            System.out.println("회원탈퇴가 완료되었습니다.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean login_check(String user_id, String user_pw) {
+        try {
+            String sql = "SELECT * FROM users WHERE user_id = ? AND user_pw = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, user_id);
+            pstmt.setString(2, user_pw);
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                rs.close();
+                pstmt.close();
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public void user_update(){
+            System.out.println("정보 수정하기를 선택하셨습니다.");
+            System.out.println("수정할 정보를 입력해주세요 : ");
+            System.out.print("비밀번호 : ");
+            users.setUser_PW(s.nextLine());
+            System.out.print("이름 : ");
+            users.setUser_Name(s.nextLine());
+            System.out.print("나이 : ");
+            users.setUser_Age(Integer.parseInt(s.nextLine()));
+            System.out.print("이메일 : ");
+            users.setUser_Email(s.nextLine());
+
+            try {
+                String sql = "UPDATE users SET user_pw = ?, user_name = ?, user_age = ?, user_email = ? WHERE user_id =?";
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+                pstmt.setString(5, login_userID);
+                pstmt.setString(1, users.getUser_PW());
+                pstmt.setString(2,users.getUser_Name());
+                pstmt.setInt(3, users.getUser_Age());
+                pstmt.setString(4, users.getUser_Email());
+                pstmt.executeUpdate();
+                pstmt.close();
+                System.out.println("회원정보가 성공적으로 수정되었습니다.");
+            } catch (SQLException e) {
+                System.out.println("회원정보 수정중 오류가 발생했습니다.");
+                e.printStackTrace();
+                exit();
+            }
+        }
+
+    public boolean login() {
+        int count = 0;
+        try {
+            while (true) {
+                System.out.println("로그인을 시작합니다.");
+                System.out.print("아이디 : ");
+                String user_id = s.nextLine();
+
+                System.out.print("비밀번호 : ");
+                String user_pw = s.nextLine();
+
+                if (login_check(user_id, user_pw)) {
+                    System.out.println("로그인 성공");
+                    login = true;
+                    login_userID = user_id;
+                    return true;
+                } else {
+                    count++;
+                    System.out.println("아이디, 비밀번호를 잘못입력하셨습니다.");
+
+                    if (count > 3){
+                        System.out.println("회원가입을 하시겠습니까 ?");
+                        System.out.println("1. 예 | 2. 아니오 ");
+                        System.out.print("선택 : ");
+                        String response = s.nextLine();
+                        switch (response) {
+                            case "1":
+                                sing_up();
+                                break;
+                            case "2":
+                                System.out.println("다시 로그인을 진행합니다.");
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    public void log_in(){
+        if (!login) {
+            while (!login) {
+                login = login();
+                if (!login) {
+                    System.out.println("로그인에 실패했습니다. 다시 시도해주세요.");
+                }
+            }
+        }
+    }
+
+    public boolean logout(){
+        login = false;
+        return false;
     }
 }
---
+++ src/report/teacher.java
@@ -0,0 +1,62 @@
+package report;
+
+public class teacher {
+    private String th_id;
+    private String th_pw;
+    private String th_class;
+    private String th_name;
+    private String th_email;
+
+
+    public String getth_id() {
+        return th_id;
+    }
+
+    public void setth_id(String th_id) {
+        this.th_id = th_id;
+    }
+
+    public String getth_pw() {
+        return th_pw;
+    }
+
+    public void setth_pw(String th_pw) {
+        this.th_pw = th_pw;
+    }
+
+    public String getth_class() {
+        return th_class;
+    }
+
+    public void setth_class(String th_class) {
+        this.th_class = th_class;
+    }
+
+    public String getth_name() {
+        return th_name;
+    }
+
+    public void setth_name(String th_name) {
+        this.th_name = th_name;
+    }
+
+    public String getth_email() {
+        return th_email;
+    }
+
+    public void setth_email(String th_email) {
+        this.th_email = th_email;
+    }
+
+    @Override
+    public String toString() {
+        return "teacher{" +
+                "th_id='" + th_id + '\'' +
+                ", th_pw='" + th_pw + '\'' +
+                ", th_class='" + th_class + '\'' +
+                ", th_name='" + th_name + '\'' +
+                ", th_email='" + th_email + '\'' +
+                '}';
+    }
+
+}
